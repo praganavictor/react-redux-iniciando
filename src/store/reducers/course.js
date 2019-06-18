515a6bd10e57344222eb6fa58b5239e569df6b6d
Victor Pragana <victormerigue@gmail.com>
2019-06-18 13:37:38 -0300

    um exemplo de como utilizar redux

---
+++ src/store/reducers/course.js
@@ -0,0 +1,45 @@
+const INITIAL_STATE = {
+  activeLesson: {},
+  activeModule: {},
+  modules: [
+    {
+      id: 1,
+      title: "Iniciando com React",
+      lessons: [
+        {
+          id: 1,
+          title: "primeira aula"
+        },
+        {
+          id: 2,
+          title: "segunda aula"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Iniciando com Redux",
+      lessons: [
+        {
+          id: 3,
+          title: "Terceira aula"
+        },
+        {
+          id: 4,
+          title: "Quarta aula"
+        }
+      ]
+    }
+  ]
+};
+
+export default function course(state = INITIAL_STATE, action) {
+  if (action.type === "TOGGLE_LESSON") {
+    return {
+      ...state,
+      activeLesson: action.lesson,
+      activeModule: action.module
+    };
+  }
+  return state;
+}
